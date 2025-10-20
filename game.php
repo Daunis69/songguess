62ed33fc8fab564b69e9b76de34551753b9dcd50
Daunis69 <berzinspavils@gmail.com>
2025-10-20 13:36:40 +0300

    gribu pakarties

--- game.php
+++ game.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <div class="head">
+        <h1 class="gmhedtxt">SONGUESS</h1>
+    </div>
   <input type="text" placeholder="Song name">
    <audio controls autoplay>
   <source src="horse.ogg" type="audio/ogg">
