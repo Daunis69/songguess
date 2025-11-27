14023669eda196ecae54d35cb0ccd750483dbf44
Daunis69 <berzinspavils@gmail.com>
2025-11-27 11:04:59 +0200

    fixed white screen on sing up

--- signup.php
+++ signup.php
@@ -1,4 +1,20 @@
-<?php
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up - Songuess</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Sign Up</h2>
+        <form method="post" action="signup.php">
+            <input type="text" name="username" placeholder="Username" required>
+            <input type="text" name="email" placeholder="Email" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <button type="submit">Sign Up</button>
+            <?php
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
@@ -37,21 +53,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Sign Up - Songuess</title>
-    <link rel="stylesheet" href="login.css">
-</head>
-<body>
-    <div class="container">
-        <h2>Sign Up</h2>
-        <form method="post" action="signup.php">
-            <input type="text" name="username" placeholder="Username" required>
-            <input type="text" name="email" placeholder="Email" required>
-            <input type="password" name="password" placeholder="Password" required>
-            <button type="submit">Sign Up</button>
         </form>
         <div class="link">
             <span>Already have an account? <a href="login.php">Login</a></span>
